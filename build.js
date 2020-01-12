d15830c509b5c407e6ae436d4e7afd72fcfc880b
Vidar Eldøy <vidar@eldoy.com>
2020-01-12 21:14:54 +0100

    Added build script

---
+++ build.js
@@ -0,0 +1,12 @@
+module.exports = async function() {
+  return {
+    host: 'http://localhost:5000',
+    urls: [
+      '/',
+      '/about.html',
+      '/contact.html',
+      '/projects.html',
+      '/services.html'
+    ]
+  }
+}
