0557c47de0a67348530cfb1786f75ea9f19165e9
Vidar Eldøy <vidar@eldoy.com>
2020-01-15 21:17:05 +0100

    Add logo example

--- app/pages/index.js
+++ app/pages/index.js
@@ -2,13 +2,19 @@ module.exports = async function($) {
   $.page.title = 'Home'
 
   function handleSubmit() {
-  console.log("Clicked")
+    console.log("Clicked")
   }
 
   return /* html */`
     <div class="wrapper">
+      <section class="logo">
+        <img src="/img/eldoy-draft3-web-black.svg" width="200">
+      </section>
+      <section class="logo">
+        <img src="/img/eldoy-draft2-web-black.svg" width="200">
+      </section>
       <section class="main">
-        <h1>Eldøy project lager nettsider som gir økt omsetning.</h1>
+        <h1>Eldøy Projects lager nettsider som gir økt omsetning.</h1>
         <h5 style="font-style: italic;">" Med min erfaring kan jeg hjelpe deg med å sette opp en plan på hva du virkelig trenger, slik at unødvendige deler kan plukkes bort. Etter lanseringen tilbyr jeg også utvikling, drift og støtte, slik at du kan gjøre det du er best på."</h5>
       </section>
     </div>
