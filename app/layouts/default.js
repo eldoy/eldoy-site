9257cc55423c8befb4ddd58df66c37a7d5cffacd
Vidar Eldøy <vidar@eldoy.com>
2020-01-17 06:41:27 +0100

    Removed unused js and font

--- app/layouts/default.js
+++ app/layouts/default.js
@@ -11,12 +11,10 @@ module.exports = async function($) {
     <html lang="en">
       <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
-        <title>${ $.page.title || 'Untitled' }</title>
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700&display=swap" rel="stylesheet">
+        <title>${ $.page.title || 'Eldøy Projects' }</title>
         <link rel="stylesheet" href="/css/app.css" type="text/css">
         <link rel="stylesheet" href="/css/variables.css" type="text/css">
         <link rel="icon" type="image/png" href="/img/favicon.png">
-        <script src="/js/haka-min.js"></script>
       </head>
       <body>
         <header>
@@ -25,16 +23,14 @@ module.exports = async function($) {
               <img class="navbar-logo-badge" src="/img/badge-white.svg" alt="Eldøy logo badge">
               <img class="navbar-logo-text" src="/img/textonly.svg" alt="Eldøy logo text">
             </a>
-            <!-- <a class="navlink" href="/about.html">Om oss</a> -->
-            <!-- <a class="navlink" href="/services.html">Tjenester</a> -->
           </nav>
         </header>
         <div class="main">${ $.page.content }</div>
           <footer>
-            <p>Made by Eldøy Projects</p>
+            <p>Laget av Eldøy Projects, Oslo, Norge</p>
           </footer>
         </div>
-        <script>${ init }; init()</script>
+        <script>${ init };init()</script>
       </body>
     </html>`
 }
