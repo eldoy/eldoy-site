b74dd7ae29b550f06d68f9c9dcadf01327d611d9
Vidar Eldøy <vidar@eldoy.com>
2021-03-06 11:25:08 +0100

    Update to latest waveorb

--- app/layouts/default.js
+++ app/layouts/default.js
@@ -1,31 +1,32 @@
 module.exports = async function($) {
   return /* html */`
     <!doctype html>
-    <html lang="${ $.lang }">
+    <html lang="${$.lang}">
       <head>
         <meta http-equiv="content-type" content="text/html; charset=utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="description" content="${ $.page.description }">
-        <title>${ $.page.title || '♥' } - Eldøy Projects</title>
-        <link rel="stylesheet" href="/css/app.css" type="text/css">
+        <title>${$.page.title || '♥'} - Eldøy Projects</title>
         <link rel="icon" type="image/png" href="/img/favicon.png">
+        ${$.script('/bundle.js')}
+        ${$.style('/bundle.css')}
       </head>
       <body>
         <script>document.cookie='lang=;expires=Thu, 01 Jan 1970 00:00:01 GMT;'</script>
         <div class="lang">
-          <a href="${ $.link(`no#${ $.page.name }`) }">NO</a>
+          <a href="${$.link(`no@${$.page.name}`)}">NO</a>
           <span class="split">/</span>
-          <a href="${ $.link(`en#${ $.page.name }`) }">EN</a>
+          <a href="${$.link(`en@${$.page.name}`)}">EN</a>
         </div>
         <script>
           document.querySelectorAll('.lang a').forEach(function(a) {
             if (a.pathname == location.pathname) a.classList.add('active')
           })
         </script>
-        <div class="main">${ $.page.content }</div>
+        <div class="main">${$.page.content}</div>
         <footer>
           <img src="/img/eldoy-logo-badge.svg" alt="Eldøy footer logo">
-          <p>${ $.t('layout.made_by') }</p>
+          <p>${$.t('layout.made_by')}</p>
         </footer>
       </body>
     </html>`
